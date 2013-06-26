ef307a28380a45a5eeac4930e91d0e6d89a15539
PACIFICA <MOOKBOOK13@Taruns-MacBook-Air.local>
2013-06-26 18:31:22 +1000

    updated stylesheet and main.rb file

--- main.rb
+++ main.rb
@@ -1,33 +1,49 @@
 require 'pry'
-require 'json'
-require 'httparty'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-
-get '/d' do
-  @message = 'APPL is quoted at $457' #generate this using yahoofinance
-  erb :results
-end
+require 'JSON'
+require 'HTTParty'
 
 get '/' do
-  @abbrev = params[:abbrev]
-  if @abbrev.nil? or @abbrev.empty?
-    @quote = ''
-  else
-    @abbrev = @abbrev.upcase
-    @quote = YahooFinance::get_quotes(YahooFinance::StandardQuote, @abbrev)[@abbrev].lastTrade
-  end
-  erb :results
+  erb :home
+end
+post '/results' do
+@moviename = params[:moviename]
+@filminfo = JSON(HTTParty.get("http://www.omdbapi.com/?i=&t=#{@moviename}"))
+@year = (@filminfo['Year'])
+erb	:results
 end
 
 
-  #if !@abbrev.nil? && !@abbrev.empty?
-   # @quote = YahooFinance::get_quotes(YahooFinance::StandardQuote, @abbrev.upcase)[@abbrev.upcase].lastTrade
-  #else
-    #@quote = ''
-  #end
-  #erb :quote
-#end
 
+# receives 
 
+# get '/about' do
+#   erb: about
+# end
+# get '/faq' do
+#   erb: faq
+# end
+# get '/results' do
+#   erb: results
+# end
 
+# @film = HTTParty.get('http://www.omdbapi.com/?i=&t=Cinderfella').gsub("'",'') #substitute title
+# puts film
+
+# film = JSON(film)
+# #puts film['Title']
+
+#   @symbol = params[:symbol]
+#   if (@symbol.nil? || @symbol == '')
+#     @message = ''
+#   else
+#     @symbol = @symbol.upcase
+#     result =  YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)
+#     if (result != nil)
+#       price = result[@symbol].lastTrade
+#     @message = "#{@symbol} is trading at #{price}"
+#     end
+#   end
+#   erb :quote
+# end
